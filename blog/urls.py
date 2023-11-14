9fc31b83d77dade7a97f6ae3974f920b5aa9ca07
mbaus24 <baus.martin@yahoo.fr>
2023-11-14 17:02:02 +0100

    modif ui

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.character_list, name='character_list'),
-    path('character/<int:pk>/', views.character_detail, name='character_detail'),
+    path('character/<int:id_character>/', views.character_detail, name='character_detail'),
     path('character/<str:id_character>/?<str:message>', views.character_detail, name='character_detail_mes'),
 ]
